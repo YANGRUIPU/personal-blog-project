ebdfbdbaf601043827dde6cc54d23daa55aaad22
yang ruipy <yangruipu252@gmail.com>
2023-05-25 09:11:37 +0900

    添加项目前端部分

---
+++ blog-1/src/main/resources/static/styleManagmentSyetem.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+var button = document.getElementById('Add Blog');
+button.addEventListener('click', function() {
+window.location.href = 'blogEditor.html'; // 将跳转网址替换为目标网页的URL
+});
+
+var button = document.getElementById('Edit Blog');
+button.addEventListener('click', function() {
+window.location.href = 'blogEditor.html'; // 将跳转网址替换为目标网页的URL
+});
